6f94257df4b275d7f2fec385049381725c7d443c
Wasp3r <andrzejosowski93@gmail.com>
2020-05-20 21:08:08 +0200

    Chat bouble and font sizes

---
+++ LearningChatApp/Controls/Chat/ChatMessage/ChatMessageListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LearningChatApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ChatMessageListControl.xaml
+    /// </summary>
+    public partial class ChatMessageListControl : UserControl
+    {
+        public ChatMessageListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
