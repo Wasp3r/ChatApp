ae4bad9dc34d4e8efa551d08c37ecc386eb1b999
Wasp3r <andrzejosowski93@gmail.com>
2020-05-24 12:12:59 +0200

    Fixed messages alignment and updated .gitignore

--- LearningChatApp/ValueConverters/SentByMeToAligmentConverter.cs
+++ LearningChatApp/ValueConverters/SentByMeToAligmentConverter.cs
@@ -9,9 +9,9 @@ namespace LearningChatApp
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (parameter != null)
-                return (bool)value ? HorizontalAlignment.Right : HorizontalAlignment.Left;
-            else
                 return (bool)value ? HorizontalAlignment.Left : HorizontalAlignment.Right;
+            else
+                return (bool)value ? HorizontalAlignment.Right : HorizontalAlignment.Left;
         }
 
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
